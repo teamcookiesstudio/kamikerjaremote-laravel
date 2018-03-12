efe1bac71a7a73bd63d8eb9189d178ceff569fdd
unknown <anhsbolic@gmail.com>
2018-03-12 10:32:38 +0700

    Create & migrate Profiles

---
+++ database/migrations/2018_03_12_032446_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('current_position');
+            $table->string('location');
+            $table->text('summary');
+            $table->string('website')->nullable();
+            $table->string('url_photo_profile')->nullable();                      
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
