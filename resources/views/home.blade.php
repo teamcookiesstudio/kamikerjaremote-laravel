3e8fd97c614c69bd66c4ace5ea64ea621a128e94
unknown <anhsbolic@gmail.com>
2018-03-12 22:04:40 +0700

    create profile page as home page (without skillset & portofolio)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.main_layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <p>{{$user->first_name}}</p>
-                    <p>{{$user->last_name}}</p>
-                    <p>{{$user->profile->occupation ?? null}}</p>
-                    <p>{{$user->profile->location ?? null}}</p>
-                    <p>{{$user->profile->summary ?? null}}</p>
-                    <p>{{$user->profile->website ?? null}}</p>
-                    <p><a href="{{ route('profiles.edit') }}">Ubah profile</a></p>
-                </div>
-            </div>
+<div class="user-cover">
+    <div class="container">
+      <div class="row center-xs">
+        <div class="col-xs-11 col-md-12">
+          <img src="https://randomuser.me/api/portraits/men/51.jpg">
+          <a class="btn btn-outline btn-sm" href="{{ route('profiles.edit') }}">Ubah profile</a>
         </div>
+      </div>
+    </div>
+</div>
+<div class="container user">
+    <div class="row center-xs">
+      <div class="user-info col-xs-11 col-md-8">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <strong class="name">{{$user->first_name}} {{$user->last_name}}</strong>
+        <span class="job">{{$user->profile->occupation ?? null}}</span>
+        <span class="location">{{$user->profile->location ?? null}}</span>
+        <span class="about">{{$user->profile->summary ?? null}}</span>
+        <a href="{{$user->profile->website ?? null}}" target="_blank" class="link">{{$user->profile->website ?? null}}</a>
+      </div>
     </div>
 </div>
 @endsection
