4ed023f3dcc6e8844f7ae58a1f78ed734a5ba5d8
Aslam <iupin5212@gmail.com>
2018-04-29 13:04:58 +0700

    Add dummy search freelance

--- app/Http/Controllers/Admin/BrowseController.php
+++ app/Http/Controllers/Admin/BrowseController.php
@@ -4,15 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository;
+use App\Repositories\ProfileRepository;
 
 class BrowseController extends Controller
 {
+    protected $userRepo;
+
+    protected $profileRepo;
+
+    public function __construct(UserRepository $userRepo, ProfileRepository $profileRepo)
+    {
+        $this->userRepo = $userRepo;
+        $this->profileRepo = $profileRepo;
+    }
+    
     public function index(Request $request)
     {
-        $result = \App\Models\Profile::whereHas('skillsets', function($query) {
+        $result = $this->profileRepo->whereHas('skillsets', function($query) {
             $query->where('skill_set_name', 'PHP');
         })->get();
 
-        return view('admins.browse.browse', compact('result'));
+        $model = $this->userRepo->paginate(10);
+
+        return view('admins.browse.browse', compact('model'));
     }
 }
